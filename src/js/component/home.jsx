e051a1cee6e96bc4d98db395eea8019e425d0cda
andyymor <61852990+andyymor@users.noreply.github.com>
2023-12-04 21:45:52 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,11 +7,24 @@ const Home = () => {
 	const [ selectedColor, setSelectedColor] = useState("red");
 	return (
 		<div className="traffic-light">
-			O
-			<div className={"light red"+(selectedColor === "red") ? " glow" : ""}> </div>
-			<div className={"light yellow"+(selectedColor === "yellow") ? " glow" : ""}> </div>
-			<div className={"light green"+(selectedColor === "green") ? " glow" : ""}> </div>
-		</div>
+			
+			<div
+			onClick={() => selectedColor("red")} 
+			className={
+				"light red"+(selectedColor === "red" ? " glow" : "") 
+			}></div>
+			
+			<div 
+			onClick={() => selectedColor("yellow")}
+			className={ "light yellow"+(selectedColor === "yellow" ? " glow" : "")
+		}> </div>
+			
+			<div 
+			onClick={() => selectedColor("green")}
+			className={"light green"+(selectedColor === "green" ? " glow" : "")
+			}> </div>
+			
+			</div>
 	);
 };
 
